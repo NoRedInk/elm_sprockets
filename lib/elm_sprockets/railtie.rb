17a85a522d0c611d05fb2d2e2f49d20af9a48492
Michael Glass <me@mike.is>
2017-02-26 23:48:11 -0800

    move mime type into a constant

--- lib/elm_sprockets/railtie.rb
+++ lib/elm_sprockets/railtie.rb
@@ -1,12 +1,14 @@
 module ElmSprockets
   class Railtie < Rails::Railtie
+    MIME_TYPE = 'text/x-elm'.freeze
     config.assets.configure do |env|
-      env.register_mime_type 'text/x-elm', extensions: ['.elm']
-      env.register_preprocessor 'text/x-elm', Sprockets::DirectiveProcessor.new(comments: ["--", ["{-", "-}"]])
+      env.register_mime_type MIME_TYPE, extensions: ['.elm']
+      env.register_preprocessor MIME_TYPE, Sprockets::DirectiveProcessor.new(comments: ["--", ["{-", "-}"]])
 
-      output_dir = Rails.root.join("tmp", "cache", "assets", "elm")
-      transformer = Transformer.new(output_dir: output_dir, compile_dir: Rails.root)
-      env.register_transformer 'text/x-elm', 'application/javascript', transformer
+      output_dir = Rails.root.join('tmp', 'cache', 'assets', 'elm')
+      transformer =
+        Transformer.new(output_dir: output_dir, compile_dir: Rails.root)
+      env.register_transformer(MIME_TYPE, 'application/javascript', transformer)
     end
   end
 end
